f62bf8e20a93ea109e2ba15343650954197ef3e0
smailnajim <smailnajim>
2025-02-03 22:20:09 +0100

    start

--- App/controllers/HomeController.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class HomeController {
-    public function index() {
-        include_once "./App/View/Home.php";
-    }
-}
--- public/index.php
+++ public/index.php
@@ -1,15 +1,43 @@
 <?php
+require "./../vendor/autoload.php";
 
-include_once "./../App/controllers/HomeController.php";
-
-
+use App\controllers\Front\HomeController;
+use App\controllers\Front\LoginController;
+use App\controllers\Front\RegisterController;
 
 
 $path = $_SERVER['REQUEST_URI'];
-echo $path;
+
+$path = str_replace('/MVC/public/', '', $path);
+
 switch ($path){
-    case '/':
-        $homeController = new HomeController;
+    case '':
+        $homeController = new HomeController();
         $homeController->index();
         break;
+
+    case 'login':
+        $loginController = new LoginController;
+        $loginController->viewLogin();
+        break;
+
+    case 'register':
+        $registerController = new RegisterController;
+        $registerController->viewRegister();
+        break;
+    default:
+        echo 'your path :'.$path;
 }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $path ?></title>
+</head>
+<body>
+    
+</body>
+</html>
